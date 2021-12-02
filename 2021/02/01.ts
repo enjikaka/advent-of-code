5c62858201a8320acce1885089e300bb008b4bf0
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-12-02 14:30:25 +0100

    Descructer

--- 2021/02/01.ts
+++ 2021/02/01.ts
@@ -3,9 +3,9 @@ const text = await Deno.readTextFile('./02/input.txt');
 const [x, y] = text
   .split('\n')
   .map(line => line.split(' '))
-  .reduce(([x, y], sline) => ([
-    x + (sline[0].includes('forward') ? + sline[1] : 0),
-    y + (sline[0].includes('up') ? - sline[1] : 0) + (sline[0].includes('down') ? + sline[1] : 0)
+  .reduce(([x, y], [dir, val]) => ([
+    x + (dir.includes('forward') ? + val : 0),
+    y + (dir.includes('up') ? - val : 0) + (dir.includes('down') ? + val : 0)
   ]), [0, 0]);
 
 console.log(x * y);
