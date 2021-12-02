0eda230ca218024b16018db67fff03e6a976d8c0
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-12-02 14:29:44 +0100

    Reuse spli ;P

--- 2021/02/01.ts
+++ 2021/02/01.ts
@@ -2,9 +2,10 @@ const text = await Deno.readTextFile('./02/input.txt');
 
 const [x, y] = text
   .split('\n')
-  .reduce(([x, y], line) => ([
-    x + (line.split(' ')[0].includes('forward') ? + line.split(' ')[1] : 0),
-    y + (line.split(' ')[0].includes('up') ? - line.split(' ')[1] : 0) + (line.split(' ')[0].includes('down') ? + line.split(' ')[1] : 0)
+  .map(line => line.split(' '))
+  .reduce(([x, y], sline) => ([
+    x + (sline[0].includes('forward') ? + sline[1] : 0),
+    y + (sline[0].includes('up') ? - sline[1] : 0) + (sline[0].includes('down') ? + sline[1] : 0)
   ]), [0, 0]);
 
 console.log(x * y);
