70d7b93243cfad3da5b1b9c9b68c04208dc1187b
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2021-12-02 14:36:24 +0100

    Part 2

---
+++ 2021/02/02.ts
@@ -0,0 +1,12 @@
+const text = await Deno.readTextFile('./02/input.txt');
+
+const [x, y] = text
+  .split('\n')
+  .map(line => line.split(' '))
+  .reduce(([x, y, aim], [dir, val]) => ([
+    x + (dir.includes('forward') ? + val : 0),
+    y + (dir.includes('forward') ? + val * aim : 0),
+    aim + (dir.includes('up') ? - val : 0) + (dir.includes('down') ? + val : 0)
+  ]), [0, 0, 0]);
+
+console.log(x * y);
